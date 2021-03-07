bd43c867b9ef25b603148b920d1489477d2e80b8
yarisson <gemini8811@gmail.com>
2021-03-07 23:56:07 +0300

    add tooltips

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import router from './router';
 import store from './store';
 import dateFilter from './filters/date.filter';
 import currencyFilter from './filters/currency.filter';
+import tooltipDirective from './directives/tooltip.directive';
 import messagePlugin from './utils/message.plugin';
 import 'materialize-css/dist/js/materialize.min';
 
@@ -19,6 +20,7 @@ Vue.config.productionTip = false;
 Vue.use(messagePlugin);
 Vue.filter('date', dateFilter);
 Vue.filter('currency', currencyFilter);
+Vue.directive('tooltip', tooltipDirective);
 Vue.use(Vuelidate);
 Vue.component('Loader', Loader);
 
--- src/views/Planning.vue
+++ src/views/Planning.vue
@@ -1,23 +1,78 @@
 <template>
   <div>
-  <div class="page-title">
-    <h3>Планирование</h3>
-    <h4>12 212</h4>
-  </div>
-
-  <section>
-    <div>
-      <p>
-        <strong>Девушка:</strong>
-        12 122 из 14 0000
-      </p>
-      <div class="progress" >
-        <div
-            class="determinate green"
-            style="width:40%"
-        ></div>
-      </div>
+    <div class="page-title">
+      <h3>Планирование</h3>
+      <h4>{{ info.bill | currency('RUB') }}</h4>
     </div>
-  </section>
+
+    <Loader v-if="loading" />
+
+    <p class="center" v-else-if="!categories.length">категорий пока нет <router-link to="/categories">создать категорию</router-link></p>
+
+    <section v-else>
+      <div v-for="cat of categories"
+           :key="cat.id">
+        <p>
+          <strong>{{cat.title}}:</strong>
+          {{cat.spend | currency}} из {{cat.limit | currency}}
+        </p>
+        <div class="progress" v-tooltip="cat.tooltip">
+          <div
+              class="determinate"
+              :class="[cat.progressColor]"
+              :style="{width: cat.progressPercent + '%'}"
+          ></div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
+
+<script>
+import {mapGetters} from 'vuex';
+import currencyFilter from '../filters/currency.filter';
+
+export default {
+  name: 'planning',
+  data: () => ({
+    loading: true,
+    categories: [],
+  }),
+  computed: {
+    ...mapGetters(['info'])
+  },
+  async mounted() {
+    const records = await this.$store.dispatch('fetchRecords');
+    const categories = await this.$store.dispatch('fetchCategories');
+
+    this.categories = categories.map(cat => {
+      const spend = records
+        .filter(r => r.categoryId === cat.id)
+        .filter(r => r.type === 'outcome')
+        .reduce((total, record) => {
+          return total += +record.amount
+        }, 0)
+
+      const persent = (100 * spend / cat.limit)
+      const progressPercent = persent > 100 ? 100 : persent
+      const progressColor = persent < 60
+        ? 'green'
+        : persent < 100
+          ? 'yellow'
+          : 'red'
+
+      const tooltipValue = cat.limit - spend;
+      const tooltip = `${tooltipValue < 0 ? 'Превышение на' : 'Осталось'} ${currencyFilter(Math.abs(tooltipValue))}`
+
+      return {
+        ...cat,
+        progressPercent,
+        progressColor,
+        spend,
+        tooltip
+      }
+    })
+    this.loading = false;
+  },
+}
+</script>
